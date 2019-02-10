44261d06f019c88b1856a3328b9e1add21b2adf6
pradeep <pradeepfn@gmail.com>
2019-02-09 20:06:02 -0800

    perf counting

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd vacation;make clean;make;cd ..
-./bin/vacation -t1 -n4 -q60 -u90 -r1048576 -T4194304
+#./bin/vacation -t1 -n4 -q60 -u90 -r1048576 -T4194304
+perf stat -o log1 -T ./bin/vacation -t1 -n4 -q60 -u90 -r1048576 -T4194304
+perf stat -o log2 -e tx_exec.misc1,tx_exec.misc2,tx_mem.abort_capacity,tx_mem.abort_conflict ./bin/vacation -t1 -n4 -q60 -u90 -r1048576 -T4194304
